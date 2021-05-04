8d5c0f6737bdcfed9f3dcb8afabc9ca87551887b
PietroCampodonico <pietro.campodonico@outlook.it>
2021-05-04 21:46:25 +0200

    ok prime due milestones: da fare stili baloons

--- scripts/app.js
+++ scripts/app.js
@@ -2,5 +2,13 @@ const app = new Vue({
     el: "#app",
     data: {
         usersList: globalUsersList,
+        activeUser: "", 
+    },
+
+    methods: {
+        onUserClick(clickedUser) {
+            this.activeUser = clickedUser
+        }
     }
+
 })
