b2e018ba7ca93e5cd8e299fb660ffd2d1576648a
PietroCampodonico <pietro.campodonico@outlook.it>
2021-05-05 17:46:06 +0200

    Completata milestone 3

--- scripts/app.js
+++ scripts/app.js
@@ -3,12 +3,55 @@ const app = new Vue({
     data: {
         usersList: globalUsersList,
         activeUser: "", 
+        userInput: "",
+    },
+
+    computed: {
+        activeUserLastAccess() {
+            if (!this.activeUser.messages) {
+                return "";
+            }
+
+            //ci sarà bug quando eliminerremo i messaggi
+            const receivedMsg = this.activeUser.messages.filter((msg) => msg.status === 'received');
+
+            const lastMsgDate = receivedMsg[receivedMsg.length - 1].date;
+
+            return this.formatTime(lastMsgDate);
+        }
     },
 
     methods: {
         onUserClick(clickedUser) {
             this.activeUser = clickedUser
+        },
+
+        formatTime(stringDate) {
+            return moment(stringDate, "DD/MM/YYYY HH:mm:ss").format("HH:mm")
+        },
+
+        newMessage (){
+            this.activeUser.messages.push({
+                date: moment().format("DD/MM/YYYY HH:mm:ss"),
+                text: this.userInput,
+                status: 'sent'
+            });
+
+            setTimeout(() => {
+                this.activeUser.messages.push({
+                    date: moment().format("DD/MM/YYYY HH:mm:ss"),
+                    text: "Ok!",
+                    status: 'received'
+                })
+            },1000)
+
+            return this.userInput = ""
         }
-    }
+
+    },  
+    
+  mounted(){
+        moment().format("DD/MM/YYYY HH:mm:ss");
+  }
 
 })
